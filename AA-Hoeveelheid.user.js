779a80841f459108ae216801dc42686434572289
PieterCorten <pietercorten@hotmail.com>
2024-08-01 10:10:36 +0200

    Update AA-Hoeveelheid.user.js

--- AA-Hoeveelheid.user.js
+++ AA-Hoeveelheid.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AA-Hoeveelheid
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Melding soortelijke gewichten
 // @author       Pieter Corten
 // @match        https://asbestinventaris-oefen.ovam.be/*
