8964f27c1f27142adb09e44cf9ab146a08d8486e
PieterCorten <pietercorten@hotmail.com>
2024-08-12 16:44:49 +0200

    Update Auto-Abesco.user.js

--- Auto-Abesco.user.js
+++ Auto-Abesco.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-Abesco
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      1.0
 // @description  Auto Abesco Master
 // @author       Pieter Corten
 // @match        https://asbestinventaris-oefen.ovam.be/*
