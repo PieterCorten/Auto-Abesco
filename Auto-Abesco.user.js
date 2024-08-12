0acff97f5597561ec3129564d85dac0d49046504
PieterCorten <pietercorten@hotmail.com>
2024-08-12 16:44:21 +0200

    Add files via upload

---
+++ Auto-Abesco.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Auto-Abesco
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  Auto Abesco Master
+// @author       Pieter Corten
+// @match        https://asbestinventaris-oefen.ovam.be/*
+// @match        https://asbestinventaris.ovam.be/*
+// @require      https://code.jquery.com/jquery-3.7.1.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Adviesfiche-Beschrijving.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Afvalfiche-Beschrijving.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Beperkingsfiche-Beschrijving.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Bronfiche-Afvalfiche-Hoeveelheid.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Bronfiche-Primaire-Drager.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Bronfiche-Roerendgoedfiche-Notitie.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Verwijderingsmethodes-Couveuzezak.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Verwijderingsmethodes-Eenvoudige-handelingen-DHZ.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Verwijderingsmethodes-Eenvoudige-handelingen-erkend-verwijderaar.user.js
+// @require      https://github.com/PieterCorten/Auto-Abesco/raw/main/AA-Verwijderingsmethodes-Hermetische-zone.user.js
+// @updateURL    https://github.com/PieterCorten/Auto-Abesco/raw/main/Auto-Abesco.user.js
+// @downloadURL  https://github.com/PieterCorten/Auto-Abesco/raw/main/Auto-Abesco.user.js
+// @grant        none
+// ==/UserScript==
+
+console.log("Master script loaded, sub-scripts should be running.");
+
